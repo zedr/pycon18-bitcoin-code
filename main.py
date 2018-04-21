d8e175b970f2a66fd2a90b706a386d8e0d1a8a6d
Rigel Di Scala <zedr@zedr.com>
2018-04-21 22:54:30 +0200

    Configura l'endpoint datagram

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python3
+import asyncio
+import socket
+
+BIND_ADDR = "0.0.0.0"
+BROADCAST_ADDR = "192.168.0.255"
+PORT = 1337
+
+
+class _ChatProtocol(asyncio.Protocol):
+    """Un protocollo per chattare tra sconosciuti.
+    """
+    transport = None
+
+    @classmethod
+    def connection_made(cls, transport):
+        sock = transport.get_extra_info("socket")
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        cls.transport = transport
+        print("Connessi!")
+
 
 def main():
+    loop = asyncio.get_event_loop()
+    protocol = loop.create_datagram_endpoint(
+        _ChatProtocol,
+        local_addr=(BIND_ADDR, PORT)
+    )
+    loop.run_until_complete(protocol)
     while True:
         cmd, *args = input("? ").split(" ")
         if cmd.lower() == "echo":
-            print(" ".join(args))
+            message = " ".join(args)
+            print(message)
         else:
             print("Comando sconosciuto: {}".format(cmd))
 
