fdda096b0a243769a9cf1b5627d706360c5cde2a
Rigel Di Scala <zedr@zedr.com>
2018-11-10 12:07:22 +0100

    REPL

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+def say(msg):
+    print(msg)
+
+if __name__ == "__main__":
+    while True:
+        msg = input("? ")
+        try:
+            cmd, arg = msg.split(" ")
+        except ValueError:
+            pass
+        else:
+            if cmd == "say":
+                say(arg)
